a0d009a2c38cf96d54429e7cefa5191d4e112dfc
ABHI24033 <abhi24033c@gmail.com>
2025-01-18 08:48:08 +0530

    try

--- comp/Home/Banner.jsx
+++ comp/Home/Banner.jsx
@@ -10,25 +10,19 @@ const Content = {
 
 const Banner = () => {
     return (
-        <div className="mt-8">
-            <div className="flex items-center grid-cols-2 mx-auto overflow-x-hidden lg:grid md:py-14 lg:py-24 xl:py-14 lg:mt-3 xl:mt-5" data-aos="fade-right" data-aos-duration="800">
-                <div className="pr-2 w-[5rem]  bg-red-500 md:mb-14 py-14 md:py-0">
-                    <h1 className="text-3xl font-semibold text-blue-900 bg-primary xl:text-5xl lg:text-3xl">
-                        <span className="text-5xl w-[3rem]  max-w-xl ">{Content?.title}</span>
-                    </h1>
-                    <p className="py-4 text-lg text-gray-500 2xl:py-8 md:py-6 2xl:pr-5">
-                        {Content?.desc}
-                    </p>
-                    <div className="mt-4">
-                        <Link href="/" className=" px-10 py-2 rounded tracking-wider text-white bg-blue-500 md:px-8 hover:bg-blue-600 group">
-                            <span className="px-5 ">Explore our services</span>
-                        </Link>
-                    </div>
+        <div class="items-center w-10/12 grid-cols-2 mx-auto overflow-x-hidden lg:grid md:py-14 lg:py-24 xl:py-14 lg:mt-3 xl:mt-5" data-aos="fade-right" data-aos-duration="800">
+            <div class="pr-2 md:mb-14 py-14 md:py-0">
+                <h1 class="text-3xl font-semibold text-blue-900 xl:text-4xl lg:text-3xl">{Content?.title}</h1>
+                <p class="py-4 text-lg text-gray-500 2xl:py-8 md:py-6 2xl:pr-5">
+                    {Content?.subTitle}
+                </p>
+                <div class="mt-4">
+                    <a href="#contact" class="px-5 py-3 text-lg tracking-wider text-white bg-blue-500 rounded-lg md:px-8 hover:bg-blue-600 group"><span>Explore More</span> </a>
                 </div>
+            </div>
 
-                <div className="pb-10 overflow-hidden md:p-10 lg:p-0 sm:pb-0">
-                    <img id="heroImg1" className="transition-all duration-300 ease-in-out hover:scale-105 lg:w-full sm:mx-auto sm:w-4/6 sm:pb-12 lg:pb-0" src="https://bootstrapmade.com/demo/templates/FlexStart/assets/img/hero-img.png" alt="Awesome hero page image" width="500" height="488" />
-                </div>
+            <div class="pb-10 overflow-hidden md:p-10 lg:p-0 sm:pb-0">
+                <img id="heroImg1" class="transition-all duration-300 ease-in-out hover:scale-105 lg:w-full sm:mx-auto sm:w-4/6 sm:pb-12 lg:pb-0" src="https://bootstrapmade.com/demo/templates/FlexStart/assets/img/hero-img.png" alt="Awesome hero page image" width="500" height="488" />
             </div>
         </div>
     )
