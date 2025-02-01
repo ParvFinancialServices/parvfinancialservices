51605e45a184319b87a69469152a0c9026afa977
ABHI24033 <abhi24033c@gmail.com>
2025-02-01 09:28:50 +0530

    SOME CHANGES

--- middleware.js
+++ middleware.js
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   let role = request.cookies.get("role");
-  if (
-    request.nextUrl.pathname.startsWith("/dashboard/admin") &&
-    role?.value !== "Admin"
-  ) {
-    // const url = new URL()
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // if (
+  //   request.nextUrl.pathname.startsWith("/dashboard/admin") &&
+  //   role?.value !== "Admin"
+  // ) {
+  //   // const url = new URL()
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
   return NextResponse.next();
 }
