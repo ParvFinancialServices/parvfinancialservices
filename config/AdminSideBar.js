078e43d6157c0e19f6729e2f119aba7f40effa55
Abhishek Kumar <abhi24033c@gmail.com>
2025-02-09 09:53:23 +0530

    change admin sidebar

--- config/AdminSideBar.js
+++ config/AdminSideBar.js
@@ -115,6 +115,148 @@ export const AdminSidebar = {
   ],
 };
 
-export const RmSideBar=()=>{
-  
-}
+export const RMSidebar = {
+  navMain: [
+    {
+      title: "Loan Applications",
+      url: "/dashboard",
+      icon: ClipboardType,
+      isActive: true,
+      items: [
+        {
+          title: "Personal Loan",
+          url: "/dashboard/admin/forms/personal_loan",
+          icon: FileUser,
+          isActive: true,
+        },
+        {
+          title: "Business Loan",
+          url: "/admin/forms/business_loan",
+          icon: BriefcaseBusinessIcon,
+          isActive: false,
+        },
+        {
+          title: "Home Loan",
+          url: "/admin/forms/home_loan",
+          icon: HomeIcon,
+        },
+        {
+          title: "Vehicle Loan",
+          url: "/admin/forms/vehicle_loan",
+          icon: CarIcon,
+        },
+        {
+          title: "Gold Loan",
+          url: "/admin/forms/gold_loan",
+          icon: HandCoinsIcon,
+        },
+      ],
+    },
+    {
+      title: "Loans",
+      url: "#",
+      icon: Users2Icon,
+      items: [
+        {
+          title: "Category Wise",
+          url: "/dashboard/admin/view/loans/category",
+        },
+        {
+          title: "Member Wise",
+          url: "/dashboard/admin/view/loans/member",
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "Blogs",
+      url: "/admin/blogs",
+      icon: LucideNotebookPen,
+    },
+    {
+      title: "Testimonials",
+      url: "/admin/testimonials",
+      icon: MessageSquareText,
+    },
+    {
+      title: "Contact us",
+      url: "/admin/contact_data",
+      icon: Contact2Icon,
+    },
+  ],
+};
+
+export const DSASidebar = {
+  navMain: [
+    {
+      title: "Loan Applications",
+      url: "/dashboard",
+      icon: ClipboardType,
+      isActive: true,
+      items: [
+        {
+          title: "Personal Loan",
+          url: "/dashboard/admin/forms/personal_loan",
+          icon: FileUser,
+          isActive: true,
+        },
+        {
+          title: "Business Loan",
+          url: "/admin/forms/business_loan",
+          icon: BriefcaseBusinessIcon,
+          isActive: false,
+        },
+        {
+          title: "Home Loan",
+          url: "/admin/forms/home_loan",
+          icon: HomeIcon,
+        },
+        {
+          title: "Vehicle Loan",
+          url: "/admin/forms/vehicle_loan",
+          icon: CarIcon,
+        },
+        {
+          title: "Gold Loan",
+          url: "/admin/forms/gold_loan",
+          icon: HandCoinsIcon,
+        },
+      ],
+    },
+    {
+      title: "Loans",
+      url: "#",
+      icon: Users2Icon,
+      items: [
+        {
+          title: "Category Wise",
+          url: "/dashboard/admin/view/loans/category",
+        },
+        {
+          title: "Member Wise",
+          url: "/dashboard/admin/view/loans/member",
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "My Income",
+      url: "/admin/my-income",
+      icon: HandCoinsIcon,
+    },
+    // {
+    //   title: "Testimonials",
+    //   url: "/admin/testimonials",
+    //   icon: MessageSquareText,
+    // },
+    // {
+    //   title: "Contact us",
+    //   url: "/admin/contact_data",
+    //   icon: Contact2Icon,
+    // },
+  ],
+};
+
+
